2ec7d71b0631778745937e30f7299fd7f78b6ac7
greenyleaf <sdrkyj@163.com>
2025-04-11 19:33:41 +0800

    dev

--- src/test/java/org/example/sjznpeswebdemo/service/CrawlerServiceTest.java
+++ src/test/java/org/example/sjznpeswebdemo/service/CrawlerServiceTest.java
@@ -1,6 +1,8 @@
 package org.example.sjznpeswebdemo.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,5 +33,39 @@ class CrawlerServiceTest {
                 .doOnCancel(() -> log.info("cancel"))
                 .take(5)
                 .blockLast();
+    }
+
+    @Test
+    void crawlPageTest() {
+        crawlerService.crawlPage(LocalDate.parse("2025-04-10"), 1)
+                .doOnNext(pricePage -> {
+                    log.info("pricePage, {}", pricePage);
+                })
+                .block()
+        ;
+    }
+
+    @Test
+    void extractPageCountTest() {
+        crawlerService.crawlPage(LocalDate.parse("2025-04-10"), 1)
+                .map(pricePage -> {
+                    // log.info("pricePage, {}", pricePage);
+
+                    // log.info("pricePage.getContent(), {}", pricePage.getContent());
+                    return Jsoup.parse(pricePage.getContent());
+                })
+                .doOnNext(document -> {
+                    log.info("document, {}", document);
+
+                    Elements elements = document.select(".page a:last-child");
+                    log.info("elements.size, {}", elements.size());
+                    String href = elements.attr("href");
+                    log.info("href, {}", href);
+
+                    /*int count = crawlerService.extractPageCount(document);
+                    log.info("count, {}", count);*/
+                })
+                .block()
+        ;
     }
 }
