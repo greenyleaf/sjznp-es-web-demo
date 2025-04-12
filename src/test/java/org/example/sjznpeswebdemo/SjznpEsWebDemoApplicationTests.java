6bd016b5e4f5ad32b5b24af6523333bbb2106486
greenyleaf <sdrkyj@163.com>
2025-04-12 17:18:01 +0800

    edit

--- src/test/java/org/example/sjznpeswebdemo/SjznpEsWebDemoApplicationTests.java
+++ src/test/java/org/example/sjznpeswebdemo/SjznpEsWebDemoApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.sjznpeswebdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class SjznpEsWebDemoApplicationTests {
 
     @Test
