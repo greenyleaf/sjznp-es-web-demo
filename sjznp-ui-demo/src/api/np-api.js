894ca593192bf16c08491c2c8be661bb57768b09
greenyleaf <sdrkyj@163.com>
2025-04-14 11:54:14 +0800

    fmt

--- sjznp-ui-demo/src/api/np-api.js
+++ sjznp-ui-demo/src/api/np-api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // http://localhost:8080/api
 export const queryPrices = (params, signal) => axios.get('/api/', {
-        params: params,
-        signal: signal
-    })
-;
+    params: params,
+    signal: signal
+});
